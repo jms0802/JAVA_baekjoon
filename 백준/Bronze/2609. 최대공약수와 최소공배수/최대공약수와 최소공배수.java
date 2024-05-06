7ca39642868676eb0d3c65c21daa781a4cb04691
Nytail <64410285+jms0802@users.noreply.github.com>
2024-05-06 14:42:55 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+ 
+		int d = gcd(a, b);
+ 
+		System.out.println(d);
+		System.out.println(a * b / d);
+ 
+	}
+ 
+	public static int gcd(int a, int b) {
+ 
+		while (b != 0) {
+			int r = a % b;
+
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
