ff4ebbb557be1ccd397505b82eb8c169d7353977
Nytail <64410285+jms0802@users.noreply.github.com>
2024-06-01 19:11:35 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    let sum = 0;
+    let avg = 0;
+    for(let i=0;i<numbers.length;i++){
+        sum+=numbers[i];
+    }
+    avg=sum/numbers.length;
+    return avg;
+}
