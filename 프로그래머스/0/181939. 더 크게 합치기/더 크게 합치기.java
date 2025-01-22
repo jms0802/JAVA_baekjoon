5b49517520d8c9db4e4ac04d453c4d95a73eeab5
Nytail <64410285+jms0802@users.noreply.github.com>
2025-01-22 09:15:45 +0900

    [level 0] Title: 더 크게 합치기, Time: 11.17 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int a, int b) {
+        int A = Integer.parseInt(""+a+b);
+        int B = Integer.parseInt(""+b+a);
+        return Math.max(A,B);
+    }
+}
