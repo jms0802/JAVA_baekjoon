3dc9f08bed1d05ef69876e06efafcb2706d74072
Nytail <64410285+jms0802@users.noreply.github.com>
2025-02-06 17:38:31 +0900

    [Bronze III] Title: ACM 호텔, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -5,24 +5,24 @@ public class Main
 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
 		
-		while (T!=0) {
-		    T--;
-		    
-		    String[] str = br.readLine().split(" ");
+		for(int i = 0; i < T; i++) {
+		    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-    		int H = Integer.parseInt(str[0]);
-    		int W = Integer.parseInt(str[1]);
-    		int N = Integer.parseInt(str[2]);
-    		
-    		int Y = (N % H == 0) ? H : N % H;
-    		Y *= 100;
-    		
-    		int X = (N % H == 0) ? N / H : N / H + 1;
-    		
-    		System.out.println(Y+X);
+    		int H = Integer.parseInt(st.nextToken());
+			st.nextToken();			// W 는 그냥 버린다.
+			int N = Integer.parseInt(st.nextToken());
+ 
+			if (N % H == 0) {
+				sb.append((H * 100) + (N / H)).append('\n');
+ 
+			} else {
+				sb.append(((N % H) * 100) + ((N / H) + 1)).append('\n');
+			}
 		}
+		System.out.print(sb);
 	}
 }
