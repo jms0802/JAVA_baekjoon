a1e55bceee61a76dfa69c054a8f5dfc5e7c3cfda
Nytail <64410285+jms0802@users.noreply.github.com>
2025-02-06 15:38:45 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = br.readLine().split(" ");
+		double a = Double.parseDouble(str[0]);
+		double b = Double.parseDouble(str[1]);
+		
+		System.out.println(a/b);
+	}
+}
