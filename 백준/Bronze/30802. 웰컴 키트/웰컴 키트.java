67334d9a3fe1c7c2d70df43ea99a91fa6048f631
Nytail <64410285+jms0802@users.noreply.github.com>
2025-02-07 14:58:20 +0900

    [Bronze III] Title: 웰컴 키트, Time: 436 ms, Memory: 16056 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] size = br.readLine().split(" ");
+		String[] str = br.readLine().split(" ");
+		int T = Integer.parseInt(str[0]);
+		int P = Integer.parseInt(str[1]);
+		
+		//티셔츠 묶음
+		int Tsum = 0;
+		for(String s : size) {
+		    int ss = Integer.parseInt(s);
+		    while(ss > 0) {
+		        Tsum++;
+		        ss -= T;
+		    }
+		}
+		
+		//연필 묶음
+		int share = N / P;
+		int rest = N % P;
+		
+		System.out.println(Tsum);
+		System.out.println(share + " " + rest);
+	}
+}
