8e8eaa5cc0ad048ea12b46ee1212edfede2fa2b0
Nytail <64410285+jms0802@users.noreply.github.com>
2025-02-20 10:47:35 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 180 ms, Memory: 19488 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+		    String str = br.readLine();
+		    
+		    if(str.equals(".")) break;
+		    sb.append(checkStr(str)).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static String checkStr(String str) {
+	    Stack<Character> stack = new Stack<>();
+	    
+	    for(int i=0; i<str.length(); i++){
+	        char c = str.charAt(i);
+	        
+	        if(c == '(' || c == '[') {
+	            stack.push(c);
+	        } else if(c == ')') {
+	            if(stack.isEmpty() || stack.peek() != '(')
+	                return "no";
+	            else stack.pop();
+	        } else if(c == ']') {
+	            if(stack.isEmpty() || stack.peek() != '[')
+	                return "no";
+	            else stack.pop();
+	        }
+	    }
+	    if(stack.isEmpty()) return "yes";
+	    else return "no";
+	}
+}
