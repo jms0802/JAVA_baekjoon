d43ce71de299b775832487ecb95c586387ac94aa
Nytail <64410285+jms0802@users.noreply.github.com>
2025-02-21 09:06:51 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 86372 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+ 
+		int num = 666;
+		int count = 1;
+        
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		sb.append(num);
+		System.out.println(sb);
+	}
+}
