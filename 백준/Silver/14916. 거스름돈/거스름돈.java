d7944102c35d26cc79f881925d926f019dbc18c8
Nytail <64410285+jms0802@users.noreply.github.com>
2025-02-26 11:03:36 +0900

    [Silver V] Title: 거스름돈, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -9,16 +9,15 @@ public class Main
 		
         int N = Integer.parseInt(br.readLine());
         
-        int[] coins = {5,2};
         int count = 0;
         
         while(true){
-             if(N%coins[0] == 0){
-                count += N/coins[0];
+             if(N%5 == 0){
+                count += N/5;
                 sb.append(count);
                 break;
              } else{
-                N -= coins[1];
+                N -= 2;
                 count++;
              }
              
